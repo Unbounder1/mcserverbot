d5403022e9b4c804b26bba8109c12567fd532571
Unbounder1 <ryadon12@gmail.com>
2022-07-09 23:16:43 -0700

    fixed errors

--- cloudscript.py
+++ cloudscript.py
@@ -3,11 +3,13 @@ import os
 from tinydb import TinyDB, where, Query
 from tinydb import where
 from dotenv import load_dotenv
-
+import vt
 load_dotenv()
 token = os.getenv('CLOUDFLARE_TOKEN')
 domain = os.getenv('DOMAIN')
 ip = os.getenv('PUBLIC_IP')
+CLIENT_TOKEN = os.getenv('VIRUSTOTAL_TOKEN')
+
 cf = CloudFlare.CloudFlare(token=token)
 
 def create(inputname: str, guildid: int, port: int):
@@ -34,7 +36,7 @@ def delete(inputname: str, guildid: int):
 
     zone = cf.zones.get(params={'name': domain})
     zone_id = zone[0]['id']
-    dns_id=cf.zones.dns_records.get(zone_id, params={'type': 'A', 'name':name})[0]['id']
+    dns_id=cf.zones.dns_records.get(zone_id, params={'type': 'A', 'name':name + "." + domain})[0]['id']
 
     cf.zones.dns_records.delete(zone_id, dns_id)
     return
@@ -44,4 +46,17 @@ def findip(inputname: str, guildid: int):
     prefix = db.search(query['guildId'] == guildid)[0]['domainprefix']
     name = prefix + "-" + inputname + "." + domain
     return name
-
+def virustest(url: str):
+    with vt.Client(CLIENT_TOKEN) as client:
+        analysis = client.scan_url(url)
+        while True:
+            analysis = client.get_object("/analyses/{}", analysis.id)
+            if analysis.status == "completed":
+                break
+        if analysis.stats['malicious'] > 0:
+            return "Please contact bot owner to install"
+        elif analysis.stats['suspicious'] > 5:
+            return "Please contact bot owner to install"
+        else:
+            return '1'
+        return "The link does not seem to be valid or something went wrong"
--- mc.py
+++ mc.py
@@ -5,13 +5,17 @@ from tinydb import TinyDB, Query, where
 from podman import PodmanClient
 from dotenv import load_dotenv
 from time import sleep
-
+import asyncio
+import nest_asyncio
+nest_asyncio.apply()
+import vt
 import podscript
 import cloudscript
 import os
 load_dotenv()
 
 uri = os.getenv('URI')
+CLIENT_TOKEN = os.getenv('VIRUSTOTAL_TOKEN')
 class MC(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -23,44 +27,119 @@ class MC(commands.Cog):
         if self.get(ctx, name):
             await ctx.send('Server with name already exists.')
             return
-            
-        #__checking arguments__
+        
         mainenv = {'VERSION': None,'MAX_MEMORY': None}
+        #__setting up the universal env variables__
         if len(args)>0 and args!="0":
-            arglist = args.split()
-            for a in arglist:
+            argslist = args.split()
+            for a in argslist:
                 temp = a.split("=")
-                if temp[0] in mainenv:
-                    mainenv[temp[0]]=temp[1]
-                else:
-                    await ctx.send("Please check your spelling for " + temp[0])
-                    return
-
+                if temp[0] == "MAX_MEMORY":
+                    try:
+                        if 2<int(temp[1])<8:
+                            mainenv[temp[0]]=str(temp[1]) + "G"
+                        else:
+                            raise OverflowError
+                    except OverflowError:
+                        await ctx.send("Memory can only be set between 2 and 8 gigabytes")
+                        return
+                elif temp[0] == 'VERSION':
+                     mainenv[temp[0]]=temp[1]
+        
         defaultenv = {
-                'GUI': "false", 
-                'EULA': "true", 
-                'INIT_MEMORY': "1G",
+                    'GUI': "false", 
+                    'EULA': "true", 
+                    'INIT_MEMORY': "1G",
 
-                'OVERRIDE_SERVER_PROPERTIES' : "true", 
-                'REPLACE_ENV_IN_PLACE': "false",
-                'OVERRIDE_OPS': "false",
-                
-                #RCON
-                'ENABLE_RCON': "true",
-                'RCON_PASSWORD': "minecraft", #Advised to change this
-                #autopause stuff
+                    'OVERRIDE_SERVER_PROPERTIES' : "true", 
+                    'REPLACE_ENV_IN_PLACE': "false",
+                    'OVERRIDE_OPS': "false",
+                    'OVERRIDE_WHITELIST': "false",
+                    
+                    #RCON
+                    'ENABLE_RCON': "true",
+                    'RCON_PASSWORD': "minecraft", #Advised to change this
+                    #autopause stuff
 
-                'MAX_TICK_TIME' : "-1",
-                'ENABLE_AUTOPAUSE': "true",
-                'AUTOPAUSE_TIMEOUT_EST': "3600",
-                'AUTOPAUSE_KNOCK_INTERFACE' : "eno1",
+                    'MAX_TICK_TIME' : "-1",
+                    'ENABLE_AUTOPAUSE': "true",
+                    'AUTOPAUSE_TIMEOUT_EST': "3600",
+                    'AUTOPAUSE_KNOCK_INTERFACE' : "eno1",
 
-                #autostop stuff
-                'ENABLE_AUTOSTOP': "false",
-                'AUTOSTOP_TIMEOUT_EST': "172800" # 2 days
+                    #autostop stuff
+                    'ENABLE_AUTOSTOP': "false",
+                    'AUTOSTOP_TIMEOUT_EST': "172800" # 2 days
+            }
+        
+        #__setting up the specific environmental variables__
+        if mctype.lower() == "vanilla":
+            env = {**defaultenv, **mainenv}
+        elif (mctype.lower() == "custom") or (mctype.lower() == "paper") or (mctype.lower() == "bukkit") or (mctype.lower() == "spigot"):
+            linkpropenv = dict.fromkeys(['DATAPACKS','ICON','WORLD'], None)
+            otherpropenv = dict.fromkeys(['VANILLATWEAKS_SHARECODE','SPIGET_RESOURCES'], None)
+            seed = None
 
-        }
-        env = {**defaultenv, **mainenv}
+            if len(args)>0 and args!="0":
+                arglist = args.split()
+                for a in arglist:
+                    temp = a.split("=")
+                    #__malware/validity checker for links
+                    if temp[0] in linkpropenv:
+                        analysis = cloudscript.virustest(temp[1])
+                        if analysis == '1':
+                            linkpropenv[temp[0]]=temp[1]
+                        else:
+                            await ctx.send(analysis)
+                    elif temp[0] in otherpropenv:
+                        otherpropenv[temp[0]]=temp[1]
+                    elif temp[0] == "SEED":
+                        seed = temp[1]
+                    else:
+                        await ctx.send("Please check your spelling for " + temp[0])
+                        return           
+            # with vt.Client(CLIENT_TOKEN) as client:
+            #     if len(args)>0 and args!="0":
+            #         arglist = args.split()
+            #         for a in arglist:
+            #             temp = a.split("=")
+            #             #__malware/validity checker for links
+            #             if temp[0] in linkpropenv:
+            #                 analysis = client.scan_url(temp[1])
+            #                 try:
+            #                     if analysis.stats['malicious'] > 0:
+            #                         await ctx.send("Please contact bot owner to install")
+            #                         return
+            #                     elif analysis.stats['suspicious'] > 5:
+            #                         ctx.send("Please contact bot owner to install")
+            #                         return
+            #                     else:
+            #                         linkpropenv[temp[0]]=temp[1]
+            #                 except:
+            #                     await ctx.send("The link does not seem to be valid or something went wrong")
+            #                     return
+            #             elif temp[0] in otherpropenv:
+            #                 otherpropenv[temp[0]]=temp[1]
+            #             elif temp[0] == "SEED":
+            #                 seed = temp[1]
+            #             else:
+            #                 await ctx.send("Please check your spelling for " + temp[0])
+            #                 return           
+                env = {**defaultenv, **mainenv, **linkpropenv, **otherpropenv, 'SEED': seed, 'TYPE': mctype.upper()}
+
+
+        #__checking universal arguments__
+        
+        # if len(args)>0 and args!="0":
+        #     arglist = args.split()
+        #     for a in arglist:
+        #         temp = a.split("=")
+        #         if temp[0] in mainenv:
+        #             mainenv[temp[0]]=temp[1]
+        #         else:
+        #             await ctx.send("Please check your spelling for " + temp[0])
+        #             return
+
+
         #add modpack stuff later --------------------------
 
         port = None
@@ -76,13 +155,14 @@ class MC(commands.Cog):
         'serverId': ctx.guild.id,
         'name': name,
         'port': port,
+        'type': mctype
         })
 
 
         await ctx.send(f'Server with name "{name}" with added.')
         processname = name + "." + str(ctx.guild.id)
-        #__starting the podman container__
 
+        #__starting the podman container__
         if podscript.create(processname, env, port)==0:
             await ctx.send("Something went wrong. Check your spelling and try again")
             self.db.remove((where('serverId') == ctx.guild.id) & (where('name') == name))
@@ -90,21 +170,22 @@ class MC(commands.Cog):
         
         finalip = cloudscript.create(name, ctx.guild.id, port)
 
+        #__sending server status messages__
         with PodmanClient(base_url=uri) as client:
                 message = await ctx.send ("Starting the Minecraft server")
                 process=client.containers.get(processname)
                 is_starting=is_loading=is_finishing=True
                 while is_starting:
                     sleep(1)
-                    for i in process.logs(since=2):
-                        if i.decode('utf-8').find("Unpacking") != -1:
+                    for i in process.logs(since=4):
+                        if i.decode('utf-8').find("[init]") != -1:
                             await message.edit (content = "Started the Minecraft Server")
                             is_starting= False
                             break         
                 while is_loading:
                     sleep(1)
-                    for i in process.logs(since=2):
-                        if i.decode('utf-8').find("[ServerMain/INFO]") != -1:
+                    for i in process.logs(since=4):
+                        if i.decode('utf-8').find("INFO]") != -1:
                             await ctx.send("Preparing level")
                             is_loading= False
                             break
@@ -120,7 +201,9 @@ class MC(commands.Cog):
                 await message.edit(content = f"The server is up on {finalip}")
                 return
 
-
+    # @commands.command()
+    # async def create(self, ctx: Context, help: str):
+    #     await ctx.send("help thing")
     @commands.command()
     async def set(self, ctx: Context, name: str, *, args):
         if not self.get(ctx, name):
@@ -132,10 +215,11 @@ class MC(commands.Cog):
         processname = name + "." + str(ctx.guild.id)
         intpropenv = strpropenv = specialpropenv = boolpropenv = {}
         intpropenv = dict.fromkeys(['SEED','SPAWN_PROTECTION','VIEW_DISTANCE','MAX_BUILD_HEIGHT','MAX_WORLD_SIZE','MAX_PLAYERS'])
-        strpropenv = dict.fromkeys(['OPS','SERVER_NAME','MOTD','DIFFICULTY','WORLD','MAX_MEMORY'], None)
+        strpropenv = dict.fromkeys(['OPS','SERVER_NAME','MOTD','DIFFICULTY','MAX_MEMORY'], None)
         boolpropenv = dict.fromkeys(['ENABLE_COMMAND_BLOCK','HARDCORE','WHITELIST'], None)
-        specialpropenv = dict.fromkeys(['VANILLATWEAKS_SHARECODE','DATAPACKS','SPIGET_RESOURCES','ICON',], None)
-        env = {'intpropenv':intpropenv,'strpropenv':strpropenv,'boolpropenv':boolpropenv,'specialpropenv':specialpropenv}
+        linkpropenv = dict.fromkeys(['DATAPACKS','ICON','WORLD'], None)
+        otherpropenv = dict.fromkeys(['VANILLATWEAKS_SHARECODE','SPIGET_RESOURCES'], None)
+        env = {'intpropenv':intpropenv,'strpropenv':strpropenv,'boolpropenv':boolpropenv,'linkpropenv':linkpropenv,'otherpropenv':otherpropenv}
 
 
         oldenvlst = podscript.findenv(processname)
@@ -156,18 +240,24 @@ class MC(commands.Cog):
             for a in arglist:
                 try:
                     temp = a.split('=')
-                    if temp[0] in env['intpropenv']:           
+                    if temp[0] in intpropenv:           
                         try:
                             intpropenv[temp[0]]=str(int(temp[1]))
                         except:
                             raise IndexError('did not enter a integer')
-                    elif temp[0] in env['strpropenv']:
+                    elif temp[0] in strpropenv:
                         strpropenv[temp[0]]=str(temp[1])
-                    elif temp[0] in env['boolpropenv']:
+                    elif temp[0] in boolpropenv:
                         if (temp[1].lower() == "true" or temp[1].lower() == "false"):
                             boolpropenv[temp[0]]=(temp[1])
                         else:
                             raise IndexError('did not enter a valid boolean')
+                    elif temp[0] in linkpropenv:
+                        analysis = await cloudscript.virustest(temp[1])
+                        if analysis == '1':
+                            linkpropenv[temp[0]]=temp[1]
+                    elif temp[0] in otherpropenv:
+                        otherpropenv[temp[0]]=temp[1]
                     else:
                         await ctx.send(f"Did not find {temp[0]} in valid environment variables. Please try again")
                         return
@@ -176,7 +266,7 @@ class MC(commands.Cog):
                         return
 
         #__setting the new environment variables as a new container while mounting the old volume__
-            env = {**intpropenv, **strpropenv, **specialpropenv,**boolpropenv}
+            env = {**intpropenv, **strpropenv,**boolpropenv, **linkpropenv, **otherpropenv}
             portdict = self.db.search((where('serverId') == ctx.guild.id) & (where('name') == name))
             port = portdict[0]['port']
             await ctx.send(podscript.replace(processname, env, port))
@@ -238,7 +328,7 @@ class MC(commands.Cog):
             while is_starting:
                 sleep(1)
                 for i in process.logs(since=2):
-                    if i.decode('utf-8').find("Unpacking") != -1:
+                    if i.decode('utf-8').find("[init]") != -1:
                         await message.edit (content = "Started the Minecraft Server")
                         is_starting= False
                         break         
